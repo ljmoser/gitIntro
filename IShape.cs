fc7d056ed392a72b6c450d5d930d18d971cd2dd5
Lucas Moser <moser.lucas@gmail.com>
2021-02-12 14:07:32 -0500

    Removing un-needed functions

--- IShape.cs
+++ IShape.cs
@@ -2,11 +2,8 @@ namespace CST371
 {
     interface IShape
     {
-        int AWiemels();
         int GetPerimeter();
         string ToString();
-        int LucasFunct();
-        string KyleL();
     }
 
 
